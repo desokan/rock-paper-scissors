595c14fb6861129d26f819704e84d06e3e9355d9
desokan <thefdmm@gmail.com>
2024-04-12 23:42:51 +0100

    created function in kotlin

--- src/Game.kt
+++ src/Game.kt
@@ -1,4 +1,12 @@
 fun main (args: Array<String>) {
     val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
-    println("${options[0]}, ${options[1]}")
+    val num1 = 5
+    val num2 = 22
+    val total = numSum(num1, num2)
+    println("The total is: $total")
+}
+
+fun numSum (num1: Int, num2: Int): Int {
+    val sumOfTwoNumbers = num1 + num2
+    return sumOfTwoNumbers
 }

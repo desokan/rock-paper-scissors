398e62e3e6d60cb1bf9878835bdc2493d985309f
desokan <thefdmm@gmail.com>
2024-04-13 00:03:52 +0100

    created getGameChoice function

--- src/Game.kt
+++ src/Game.kt
@@ -1,17 +1,10 @@
 fun main (args: Array<String>) {
     val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
-    val num1 = 5
-    val num2 = 22
-    val total = numSum(num1, num2)
-    println("The total is: $total")
-
-    val total2 = numSum2(num1, num2)
-    println("The total is: $total2")
+    val gameChoice = getGameChoice(options)
+    println(gameChoice)
 }
 
-fun numSum (num1: Int, num2: Int): Int {
-    val sumOfTwoNumbers = num1 + num2
-    return sumOfTwoNumbers
+fun getGameChoice(optionsParams: Array<String>): String {
+    return optionsParams[(Math.random() * optionsParams.size).toInt()]
 }
 
-fun numSum2 (num1: Int, num2: Int): Int = num1 + num2

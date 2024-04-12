f57594a0087a420d6718579fb8c4ac928e91e87a
desokan <thefdmm@gmail.com>
2024-04-12 23:50:42 +0100

    created simplified function

--- src/Game.kt
+++ src/Game.kt
@@ -4,9 +4,14 @@ fun main (args: Array<String>) {
     val num2 = 22
     val total = numSum(num1, num2)
     println("The total is: $total")
+
+    val total2 = numSum2(num1, num2)
+    println("The total is: $total2")
 }
 
 fun numSum (num1: Int, num2: Int): Int {
     val sumOfTwoNumbers = num1 + num2
     return sumOfTwoNumbers
 }
+
+fun numSum2 (num1: Int, num2: Int): Int = num1 + num2

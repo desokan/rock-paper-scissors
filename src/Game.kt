3a4a902504c2af7e7694010a344260e544bc228c
desokan <thefdmm@gmail.com>
2024-04-13 00:49:45 +0100

    created getUserChoice function

--- src/Game.kt
+++ src/Game.kt
@@ -1,15 +1,15 @@
 fun main (args: Array<String>) {
     val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
-
-    for ((item, index) in options.withIndex()) {
-        println("$item, $index")
-    }
-
-    println(gameChoice)
+    println(getUserChoice(options))
 }
 
 fun getGameChoice(optionsParams: Array<String>): String {
     return optionsParams[(Math.random() * optionsParams.size).toInt()]
+}
+
+fun getUserChoice(optionsParam: Array<String>) {
+    println("Please enter one of the following:")
+    for(item in optionsParam) println(" $item")
 }
 

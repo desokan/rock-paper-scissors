b4e7a3df640ec43853079e4eaf423a2060ecf35c
desokan <thefdmm@gmail.com>
2024-04-13 00:40:48 +0100

    created simplified getGameChoice function

--- src/Game.kt
+++ src/Game.kt
@@ -1,9 +1,15 @@
 fun main (args: Array<String>) {
     val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
+
+    for ((item, index) in options.withIndex()) {
+        println("$item, $index")
+    }
+
     println(gameChoice)
 }
 
-fun getGameChoice(optionsParams: Array<String>) = optionsParams[(Math.random() * optionsParams.size).toInt()]
-
+fun getGameChoice(optionsParams: Array<String>): String {
+    return optionsParams[(Math.random() * optionsParams.size).toInt()]
+}
 

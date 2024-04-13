293b2c09c50b29f8d8a6af799c40a77d73939c0c
desokan <thefdmm@gmail.com>
2024-04-13 10:54:01 +0100

    read user input and validate input

--- src/Game.kt
+++ src/Game.kt
@@ -2,8 +2,7 @@ fun main (args: Array<String>) {
     val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
     val userChoice  = getUserChoice(options)
-
-
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParams: Array<String>): String {
@@ -27,8 +26,20 @@ fun getUserChoice(optionsParam: Array<String>): String {
             userChoice = userInput
         }
 
-        if (!isValidChoice) println("You must enter a valid choice.")
+        if (!isValidChoice) println("You must enter a valid choice from the list below.")
     }
     return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+    //Figure out the result
+    if (userChoice == gameChoice) result = "Tie!"
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+        (userChoice == "Paper" && gameChoice == "Rock") ||
+        (userChoice == "Scissors" && gameChoice == "Paper")) result = "You win!"
+    else result = "You lose!"
+    //Print the result
+    println("You chose $userChoice. I chose $gameChoice. $result")
 }
 

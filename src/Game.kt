0c19b2f7e06f53e8f00d49919e51a1d9efeaeed0
desokan <thefdmm@gmail.com>
2024-04-13 16:54:12 +0100

    add functions to the class

--- src/Game.kt
+++ src/Game.kt
@@ -125,5 +125,19 @@ open class Animal {
     open val habitat = ""
     var hunger = 10
 
+    open fun makeNoise() {
+        println("The Animal is making a noise")
+    }
 
+    open fun eat() {
+        println("The Animal is eating")
+    }
+
+    open fun roam() {
+        println("The Animal is roaming")
+    }
+
+    fun sleep() {
+        println("The Animal is sleeping")
+    }
 }

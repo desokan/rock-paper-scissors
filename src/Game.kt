7c2ad91e3ada60e66579bf2fea9ee0a9b744c8b1
desokan <thefdmm@gmail.com>
2024-04-13 16:53:35 +0100

    create new Animal class with properties

--- src/Game.kt
+++ src/Game.kt
@@ -69,51 +69,61 @@
  * getters and arrays of objects.
  */
 
-class Dog(val name: String, weightParam: Int, breedParam: String) {
+//class Dog(val name: String, weightParam: Int, breedParam: String) {
+//
+//    init{
+//        print("Dog $name has been created. ")
+//    }
+//
+//    var activities = arrayOf("Walks")
+//    val breed = breedParam.uppercase()
+//
+//    init{
+//        println("The breed is $breed.")
+//    }
+//
+//    var weight = weightParam
+//        // get() = field * 2
+//        set(value) {
+//            if(value > 0) field = value
+//        }
+//
+//    val weightInKgs: Double
+//        get() = weight / 2.2
+//
+//    fun bark() {
+//        println(if(weight < 20) "Yip!" else "Woof!")
+//    }
+//}
 
-    init{
-        print("Dog $name has been created. ")
-    }
+//fun main(args: Array<String>) {
+//    val myDog = Dog("Fido", 70, "Mixed")
+//    myDog.bark()
+//    myDog.weight = 75
+//    println("Weight in Kgs is ${myDog.weightInKgs}")
+//    println("This is the real breed =============> ${myDog.breed}")
+//
+//    myDog.weight = -2
+//    println("Weight is ${myDog.weight}")
+//
+//    myDog.activities = arrayOf("Walks", "Fetching balls", "Frisbee")
+//    for(item in myDog.activities) {
+//        println("My dog enjoys $item")
+//    }
+//
+//    val dogs = arrayOf(Dog("Kelpie", 20, "Westie"),
+//        Dog("Ripper", 10, "Poodle"))
+//
+//    dogs[1].bark()
+//    dogs[1].weight = 15
+//    println("Weight for ${dogs[1].name} is ${dogs[1].weight}")
+//}
 
-    var activities = arrayOf("Walks")
-    val breed = breedParam.toUpperCase()
+open class Animal {
+    open val image = ""
+    open val food = ""
+    open val habitat = ""
+    var hunger = 10
 
-    init{
-        println("The breed is $breed.")
-    }
 
-    var weight = weightParam
-        // get() = field * 2
-        set(value) {
-            if(value > 0) field = value
-        }
-
-    val weightInKgs: Double
-        get() = weight / 2.2
-
-    fun bark() {
-        println(if(weight < 20) "Yip!" else "Woof!")
-    }
-}
-
-fun main(args: Array<String>) {
-    val myDog = Dog("Fido", 70, "Mixed")
-    myDog.bark()
-    myDog.weight = 75
-    println("Weight in Kgs is ${myDog.weightInKgs}")
-
-    myDog.weight = -2
-    println("Weight is ${myDog.weight}")
-
-    myDog.activities = arrayOf("Walks", "Fetching balls", "Frisbee")
-    for(item in myDog.activities) {
-        println("My dog enjoys $item")
-    }
-
-    val dogs = arrayOf(Dog("Kelpie", 20, "Westie"),
-        Dog("Ripper", 10, "Poodle"))
-
-    dogs[1].bark()
-    dogs[1].weight = 15
-    println("Weight for ${dogs[1].name} is ${dogs[1].weight}")
 }

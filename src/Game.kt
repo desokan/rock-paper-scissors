54884d6a7495f34a2eac8aebbcf2d1e8158b6ba6
desokan <thefdmm@gmail.com>
2024-04-13 10:38:30 +0100

    read user input and validate input

--- src/Game.kt
+++ src/Game.kt
@@ -1,15 +1,34 @@
 fun main (args: Array<String>) {
     val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
-    println(getUserChoice(options))
+    val userChoice  = getUserChoice(options)
+
+
 }
 
 fun getGameChoice(optionsParams: Array<String>): String {
     return optionsParams[(Math.random() * optionsParams.size).toInt()]
 }
 
-fun getUserChoice(optionsParam: Array<String>) {
-    println("Please enter one of the following:")
-    for(item in optionsParam) println(" $item")
+fun getUserChoice(optionsParam: Array<String>): String {
+    var isValidChoice = false
+    var userChoice = ""
+    while (!isValidChoice) {
+        println("Please enter one of the following:")
+        for(item in optionsParam) println(" $item")
+        println("=========")
+
+        // read user input
+        val userInput = readlnOrNull()
+
+        // Validate the user input
+        if (userInput != null && userInput in optionsParam) {
+            isValidChoice = true
+            userChoice = userInput
+        }
+
+        if (!isValidChoice) println("You must enter a valid choice.")
+    }
+    return userChoice
 }
 

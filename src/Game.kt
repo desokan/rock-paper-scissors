abb1f249e18295ce21b57bf2fe382ae82891d059
desokan <thefdmm@gmail.com>
2024-04-15 16:15:39 +0100

    added an array of Animal and Vehicle

--- src/Game.kt
+++ src/Game.kt
@@ -82,6 +82,13 @@ fun main (args: Array<String>) {
     vet.giveShot(wolf)
     vet.giveShot(hippo)
 
+    val roamables = arrayOf(Hippo(), Wolf(), Vehicle())
+    for (item in roamables) {
+        item.roam()
+        if (item is Animal) {
+            item.eat()
+        }
+    }
 
 }
 

1ebd714735fd0c94611ef8469a4b610ccfa06647
desokan <thefdmm@gmail.com>
2024-04-15 06:11:53 +0100

    make Animal class an abstract

--- src/Game.kt
+++ src/Game.kt
@@ -1,3 +1,99 @@
+abstract class Animal {
+    abstract val image: String
+    abstract val food: String
+    abstract val habitat: String
+    var hunger = 10
+
+    abstract fun makeNoise()
+    abstract fun eat()
+
+    open fun roam() {
+        println("The Animal is roaming")
+    }
+
+    fun sleep() {
+        println("The Animal is sleeping")
+    }
+}
+
+
+
+//open class Animal {
+//    open val image = ""
+//    open val food = ""
+//    open val habitat = ""
+//    var hunger = 10
+//
+//    open fun makeNoise() {
+//        println("The Animal is making a noise")
+//    }
+//
+//    open fun eat() {
+//        println("The Animal is eating")
+//    }
+//
+//    open fun roam() {
+//        println("The Animal is roaming")
+//    }
+//
+//    fun sleep() {
+//        println("The Animal is sleeping")
+//    }
+//}
+//
+//class Hippo : Animal() {
+//    override val image = "hippo.jpg"
+//    override val food = "grass"
+//    override val habitat = "water"
+//
+//    override fun makeNoise() {
+//        println("Grunt! Grunt!")
+//    }
+//
+//    override fun eat() {
+//        println("The Hippo is eating $food.")
+//    }
+//}
+//
+//open class Canine : Animal() {
+//    override fun roam() {
+//        println("The Canine is roaming")
+//    }
+//}
+//
+//class Wolf : Canine() {
+//    override val image = "wolf.jpg"
+//    override val food = "meat"
+//    override val habitat = "forests"
+//
+//    override fun makeNoise() {
+//        println("Hooooowl!")
+//    }
+//
+//    override fun eat() {
+//        println("The Wolf is eating $food")
+//    }
+//}
+//
+//class Vet {
+//    fun giveShot(animal: Animal) {
+//        animal.makeNoise()
+//    }
+//}
+//
+//fun main (args: Array<String>) {
+//    val animals = arrayOf(Hippo(), Wolf())
+//    for (animal in animals) {
+//        animal.makeNoise()
+//    }
+//
+//    val vet = Vet()
+//    val wolf = Wolf()
+//    val hippo = Hippo()
+//    vet.giveShot(wolf)
+//    vet.giveShot(hippo)
+//}
+
 //fun main () {
 //    val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
 //    val gameChoice = getGameChoice(options)
@@ -119,78 +215,3 @@
 //    println("Weight for ${dogs[1].name} is ${dogs[1].weight}")
 //}
 
-open class Animal {
-    open val image = ""
-    open val food = ""
-    open val habitat = ""
-    var hunger = 10
-
-    open fun makeNoise() {
-        println("The Animal is making a noise")
-    }
-
-    open fun eat() {
-        println("The Animal is eating")
-    }
-
-    open fun roam() {
-        println("The Animal is roaming")
-    }
-
-    fun sleep() {
-        println("The Animal is sleeping")
-    }
-}
-
-class Hippo : Animal() {
-    override val image = "hippo.jpg"
-    override val food = "grass"
-    override val habitat = "water"
-
-    override fun makeNoise() {
-        println("Grunt! Grunt!")
-    }
-
-    override fun eat() {
-        println("The Hippo is eating $food.")
-    }
-}
-
-open class Canine : Animal() {
-    override fun roam() {
-        println("The Canine is roaming")
-    }
-}
-
-class Wolf : Canine() {
-    override val image = "wolf.jpg"
-    override val food = "meat"
-    override val habitat = "forests"
-
-    override fun makeNoise() {
-        println("Hooooowl!")
-    }
-
-    override fun eat() {
-        println("The Wolf is eating $food")
-    }
-}
-
-class Vet {
-    fun giveShot(animal: Animal) {
-        animal.makeNoise()
-    }
-}
-
-fun main (args: Array<String>) {
-    val animals = arrayOf(Hippo(), Wolf())
-    for (animal in animals) {
-        animal.makeNoise()
-    }
-
-    val vet = Vet()
-    val wolf = Wolf()
-    val hippo = Hippo()
-    vet.giveShot(wolf)
-    vet.giveShot(hippo)
-}

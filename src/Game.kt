85a326e03b6410d4c4ac369203bdb44ab82aeffd
desokan <thefdmm@gmail.com>
2024-04-18 09:19:59 +0100

    creat  a new Wolf class

--- src/Game.kt
+++ src/Game.kt
@@ -1,59 +1,68 @@
-data class Recipe(
-    val title: String,
-    val mainIngredient: String,
-    val isVegetarian: Boolean = false,
-    val difficulty: String = "Easy"
-)
+class Wolf {
+    var hunger = 10
+    val food = "meat"
 
-class Mushroom(val size: Int, val isMagic: Boolean) {
-    constructor(magicParam: Boolean) : this(9, magicParam) {
-        //Code that runs when the secondary constructor is called
+    fun eat() {
+        println("The Wolf is eating $food")
     }
 }
 
-fun findRecipes(
-    title: String = "",
-    ingredient: String = "",
-    isVegetarian: Boolean = false,
-    difficulty: String = ""
-): Array<Recipe> {
-    //Code to find recipes
-    return arrayOf(Recipe(title, ingredient, isVegetarian, difficulty))
-}
-
-fun addNumbers(a: Int, b: Int) : Int {
-    return a + b
-}
-
-fun addNumbers(a: Double, b: Double) : Double {
-    return a + b
-}
-
-fun main() {
-    val r1 = Recipe("Thai Curry", "Chicken")
-    val r2 = Recipe(title = "Thai Curry", mainIngredient = "Chicken")
-    val r3 = r1.copy(title = "Chicken Bhuna")
-
-    println("r1 hash code: ${r1.hashCode()}")
-    println("r2 hash code: ${r2.hashCode()}")
-    println("r3 hash code: ${r3.hashCode()}")
-
-    println("r1 toString: $r1")
-    println("r1 == r2? ${r1 == r2}")
-    println("r1 === r2? ${r1 === r2}")
-    println("r1 == r3? ${r1 == r3}")
-
-    val (title, mainIngredient, vegetarian, difficulty) = r1
-    println("title is $title and vegetarian is $vegetarian")
-
-    val m1 = Mushroom(6, false)
-    println("m1 size is ${m1.size} and isMagic is ${m1.isMagic}")
-    val m2 = Mushroom(true)
-    println("m2 size is ${m2.size} and isMagic is ${m2.isMagic}")
-
-    println(addNumbers(2,5))
-    println(addNumbers(1.6,7.3))
-}
+//data class Recipe(
+//    val title: String,
+//    val mainIngredient: String,
+//    val isVegetarian: Boolean = false,
+//    val difficulty: String = "Easy"
+//)
+//
+//class Mushroom(val size: Int, val isMagic: Boolean) {
+//    constructor(magicParam: Boolean) : this(9, magicParam) {
+//        //Code that runs when the secondary constructor is called
+//    }
+//}
+//
+//fun findRecipes(
+//    title: String = "",
+//    ingredient: String = "",
+//    isVegetarian: Boolean = false,
+//    difficulty: String = ""
+//): Array<Recipe> {
+//    //Code to find recipes
+//    return arrayOf(Recipe(title, ingredient, isVegetarian, difficulty))
+//}
+//
+//fun addNumbers(a: Int, b: Int) : Int {
+//    return a + b
+//}
+//
+//fun addNumbers(a: Double, b: Double) : Double {
+//    return a + b
+//}
+//
+//fun main() {
+//    val r1 = Recipe("Thai Curry", "Chicken")
+//    val r2 = Recipe(title = "Thai Curry", mainIngredient = "Chicken")
+//    val r3 = r1.copy(title = "Chicken Bhuna")
+//
+//    println("r1 hash code: ${r1.hashCode()}")
+//    println("r2 hash code: ${r2.hashCode()}")
+//    println("r3 hash code: ${r3.hashCode()}")
+//
+//    println("r1 toString: $r1")
+//    println("r1 == r2? ${r1 == r2}")
+//    println("r1 === r2? ${r1 === r2}")
+//    println("r1 == r3? ${r1 == r3}")
+//
+//    val (title, mainIngredient, vegetarian, difficulty) = r1
+//    println("title is $title and vegetarian is $vegetarian")
+//
+//    val m1 = Mushroom(6, false)
+//    println("m1 size is ${m1.size} and isMagic is ${m1.isMagic}")
+//    val m2 = Mushroom(true)
+//    println("m2 size is ${m2.size} and isMagic is ${m2.isMagic}")
+//
+//    println(addNumbers(2,5))
+//    println(addNumbers(1.6,7.3))
+//}
 
 //interface Roamable {
 //    fun roam()

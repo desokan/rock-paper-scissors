a1085cd92b47f67506e870a1aff803cdc0288cd8
desokan <thefdmm@gmail.com>
2024-04-20 16:10:19 +0100

    create try-catch function

--- src/Game.kt
+++ src/Game.kt
@@ -1,48 +1,60 @@
-class Wolf {
-    var hunger = 10
-    val food = "meat"
-
-    fun eat() {
-        println("The Wolf is eating $food")
-    }
-}
-
-class MyWolf {
-    var wolf: Wolf? = Wolf()
-
-    fun myFunction() {
-        wolf?.eat()
-    }
-}
-
-fun getAlphaWolf(): Wolf? {
-    return Wolf()
-}
-
-fun main(args: Array<String>) {
-    var w: Wolf? = Wolf()
-
-    if (w != null) {
-        w.eat()
+fun myFunction(str: String) {
+    try{
+        val x = str.toInt()
+        println(x)
+    } catch(e : NumberFormatException) {
+        print("Not possible")
     }
 
-    var x = w?.hunger
-    println("The value of x is $x")
-
-    var y = w?.hunger ?: -1
-    println("The value of y is $y")
-
-    var myWolf = MyWolf()
-    myWolf?.wolf?.hunger = 8
-    println("The value of myWolf?.wolf?.hunger is ${myWolf?.wolf?.hunger}")
-
-    var myArray = arrayOf("Hi", "Hello", null)
-    for (item in myArray) {
-        item?.let { println(it) }
-    }
-
-    getAlphaWolf()?.let { it.eat() }
+    println("My function has ended")
 }
+
+//class Wolf {
+//    var hunger = 10
+//    val food = "meat"
+//
+//    fun eat() {
+//        println("The Wolf is eating $food")
+//    }
+//}
+//
+//class MyWolf {
+//    var wolf: Wolf? = Wolf()
+//
+//    fun myFunction() {
+//        wolf?.eat()
+//    }
+//}
+//
+//fun getAlphaWolf(): Wolf? {
+//    return Wolf()
+//}
+//
+//fun main(args: Array<String>) {
+//    var w: Wolf? = Wolf()
+//
+//    // w?.eat()
+//    if (w != null) {
+//        w.eat()
+//    }
+//
+//    var x = w?.hunger
+//    println("The value of x is $x")
+//
+//    var y = w?.hunger ?: -1
+//    println("The value of y is $y")
+//
+//    var myWolf = MyWolf()
+//    myWolf?.wolf?.hunger = 8
+//    println("The value of myWolf?.wolf?.hunger is ${myWolf?.wolf?.hunger}")
+//
+//    var myArray = arrayOf("Hi", "Hello", null)
+//    for (item in myArray) {
+//        item?.let { println(it) }
+//    }
+//
+//    getAlphaWolf()?.let { it.eat() }
+//}
 
 //data class Recipe(
 //    val title: String,

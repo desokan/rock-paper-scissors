25483d972c8de0a2bb9947f72c92ed25dbbc02e7
desokan <thefdmm@gmail.com>
2024-04-23 12:00:04 +0100

    created contest object

--- src/Game.kt
+++ src/Game.kt
@@ -22,6 +22,24 @@ class Contest<T: Pet>() {
     }
 }
 
+fun main(args: Array<String>) {
+    val catFuzz = Cat("Fuzz Lightyear")
+    val catKatsu = Cat("Katsu")
+    val fishFinny = Fish("Finny McGraw")
+
+    val catContest = Contest<Cat>()
+    catContest.addScore(catFuzz, 50)
+    catContest.addScore(catKatsu, 45)
+    val topCat = catContest.getWinners().first()
+    println("Cat contest winner is ${topCat.name}")
+
+    val petContest = Contest<Pet>()
+    petContest.addScore(catFuzz, 50)
+    petContest.addScore(fishFinny, 56)
+    val topPet = petContest.getWinners().first()
+    println("Pet contest winnerr is ${topPet.name}")
+}
+
 //data class Recipe(var name: String)
 //
 //fun main(args: Array<String>) {

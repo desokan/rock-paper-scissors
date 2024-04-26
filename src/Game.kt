4c37a26357f75e5efcc79d8adb71b6591b918856
desokan <thefdmm@gmail.com>
2024-04-26 19:41:59 +0100

    added a vet class

--- src/Game.kt
+++ src/Game.kt
@@ -3,6 +3,12 @@ class Cat(name: String) : Pet(name)
 class Dog(name: String) : Pet(name)
 class Fish(name: String) : Pet(name)
 
+class Vet<T: Pet> {
+    fun treat(t: T) {
+        println("Treat Pet ${t.name}")
+    }
+}
+
 class Contest<T: Pet>() {
     val scores: MutableMap<T, Int> = mutableMapOf()
 
